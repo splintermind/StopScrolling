0b12aa35c63a3b206a5fcfed0d5b827bd1e6ebb1
Josh <splintermind@gmail.com>
2018-05-22 17:58:39 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# StopScrolling
+Prevents the lists of equipment in the mech lab from resetting to the top.
+
+# Details
+Any time you drag and drop weapons, equipment, upgrades, etc. the list of items scrolls back up to the top. This should prevent that, and should also apply to buying/selling in the store as well.
+
+## Requirements
+[BattleTechModLoader](https://github.com/Mpstark/BattleTechModLoader/releases)
+
+## Install
+- [Install BTML](https://github.com/Mpstark/BattleTechModLoader)
+- Place the StopScrolling.dll in your \BATTLETECH\Mods\ directory
---
+++ StopScrolling/StopScrolling.cs
@@ -0,0 +1,93 @@
+﻿using Harmony;
+using HBS.Logging;
+using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+namespace StopScrolling
+{
+    public class StopScrolling
+    {
+
+        private static ILog m_log = HBS.Logging.Logger.GetLogger(typeof(StopScrolling).Name, LogLevel.Log);
+        private static float m_pos = 1f;
+
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("io.github.splintermind.StopScrolling");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void Log(string message)
+        {
+            m_log.Log(message);
+        }
+
+        [HarmonyPatch(typeof(BattleTech.UI.MechLabPanel), "PopulateInventory")]
+        public static class BattleTech_UI_MechLabPanel_PopulateInventory_Prefix
+        {
+            public static void Prefix(BattleTech.UI.MechLabPanel __instance)
+            {
+                StopScrolling.m_pos = -1f; //reset scroll to the top on the next re-position
+            }
+        }
+
+        [HarmonyPatch(typeof(BattleTech.UI.MechLabInventoryWidget), "ApplySorting")]
+        public static class BattleTech_UI_MechLabInventoryWidget_ApplySorting_Prefix
+        {
+            public static void Prefix(BattleTech.UI.MechLabInventoryWidget __instance)
+            {
+                if (Traverse.Create(__instance).Field("currentSort").GetValue<Comparison<BattleTech.UI.InventoryItemElement>>() != null)
+                {
+                    SaveScrollPosition(__instance);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(BattleTech.UI.MechLabInventoryWidget), "ApplySorting")]
+        public static class BattleTech_UI_MechLabInventoryWidget_ApplySorting_Postfix
+        {
+            public static void Postfix(BattleTech.UI.MechLabInventoryWidget __instance)
+            {
+                if (__instance.gameObject.activeInHierarchy)
+                {
+                    __instance.StartCoroutine(EndOfFrameScrollBarMovement(__instance));
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(BattleTech.UI.MechLabInventoryWidget), "ApplyFiltering")]
+        public static class BattleTech_UI_MechLabInventoryWidget_ApplyFiltering_Patch
+        {
+            public static void Postfix(BattleTech.UI.MechLabInventoryWidget __instance)
+            {
+                if (__instance.gameObject.activeInHierarchy)
+                {
+                    __instance.StartCoroutine(EndOfFrameScrollBarMovement(__instance));
+                }
+            }
+        }
+
+        private static IEnumerator EndOfFrameScrollBarMovement(BattleTech.UI.MechLabInventoryWidget __instance)
+        {
+            yield return new WaitForEndOfFrame();
+            Traverse.Create(__instance).Field("scrollbarArea").Property("verticalNormalizedPosition").SetValue(Mathf.Clamp(StopScrolling.m_pos, 0f, 1f));
+            yield break;
+        }
+
+        private static void SaveScrollPosition(BattleTech.UI.MechLabInventoryWidget __instance)
+        {
+            if (StopScrolling.m_pos == -1)
+            {
+                StopScrolling.m_pos = 1f;
+            }
+            else
+            {
+                StopScrolling.m_pos = Traverse.Create(__instance).Field("scrollbarArea").Property("verticalNormalizedPosition").GetValue<float>();
+            }
+            //Log($"saved scroll position {StopScrolling.pos}");
+        }
+
+    }
+}
