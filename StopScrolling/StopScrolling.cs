2c279feae6df6efca5f644cdef01bdcb31ed5ec1
Josh <splintermind@gmail.com>
2018-05-30 19:56:30 +0200

    shop selection, scrolling tweaks
    - reselects the next item in the shop when selling an entire stack
    instead
    - scrolls back to the top when changing shop filters or buy/sell
    - handled a case in the mech lab when scrolling and then filtering
    didn't keep the scroll position
    - general variable refactoring

--- StopScrolling/StopScrolling.cs
+++ StopScrolling/StopScrolling.cs
@@ -1,9 +1,12 @@
-﻿using Harmony;
+﻿using BattleTech.UI;
+using Harmony;
 using HBS.Logging;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace StopScrolling
 {
@@ -11,7 +14,8 @@ namespace StopScrolling
     {
 
         private static ILog m_log = HBS.Logging.Logger.GetLogger(typeof(StopScrolling).Name, LogLevel.Log);
-        private static float m_pos = 1f;
+        private static float m_scrollPos = 1f;
+        private static bool m_scrollAfterFilter;
 
         public static void Init()
         {
@@ -24,31 +28,69 @@ namespace StopScrolling
             m_log.Log(message);
         }
 
-        [HarmonyPatch(typeof(BattleTech.UI.MechLabPanel), "PopulateInventory")]
-        public static class BattleTech_UI_MechLabPanel_PopulateInventory_Prefix
+        #region MechLabPanel
+
+        [HarmonyPatch(typeof(MechLabPanel), "PopulateInventory")]
+        public static class BattleTech_UI_MechLabPanel_PopulateInventory_Postfix
         {
-            public static void Prefix(BattleTech.UI.MechLabPanel __instance)
+            public static void Postfix(MechLabPanel __instance)
             {
-                StopScrolling.m_pos = -1f; //reset scroll to the top on the next re-position
+                //Log("scrolling to top.");
+                m_scrollAfterFilter = true;
+                ScrollToTop(Traverse.Create(__instance).Field("inventoryWidget").GetValue<MechLabInventoryWidget>());
             }
         }
 
-        [HarmonyPatch(typeof(BattleTech.UI.MechLabInventoryWidget), "ApplySorting")]
+        #endregion MechLabPanel
+
+        #region MechLabInventoryWidget
+
+        [HarmonyPatch(typeof(MechLabInventoryWidget), "OnAddItem")]
+        public static class BattleTech_UI_MechLabInventoryWidget_OnAddItem_Prefix
+        {
+            public static void Prefix(MechLabInventoryWidget __instance)
+            {
+                m_scrollAfterFilter = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(MechLabInventoryWidget), "OnFilterButtonClicked")]
+        public static class BattleTech_UI_MechLabInventoryWidget_OnFilterButtonClicked_Prefix
+        {
+            public static void Prefix(MechLabInventoryWidget __instance)
+            {
+                m_scrollAfterFilter = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(MechLabInventoryWidget), "OnItemHoverEnter")]
+        public static class BattleTech_UI_MechLabInventoryWidget_OnItemHoverEnter_Postfix
+        {
+            public static void Postfix(MechLabInventoryWidget __instance)
+            {
+                m_scrollPos = Scrollbar(__instance).verticalNormalizedPosition;
+                //Log("saving scroll (hover)");
+
+            }
+        }
+
+        [HarmonyPatch(typeof(MechLabInventoryWidget), "ApplySorting")]
         public static class BattleTech_UI_MechLabInventoryWidget_ApplySorting_Prefix
         {
-            public static void Prefix(BattleTech.UI.MechLabInventoryWidget __instance)
+            public static void Prefix(MechLabInventoryWidget __instance)
             {
-                if (Traverse.Create(__instance).Field("currentSort").GetValue<Comparison<BattleTech.UI.InventoryItemElement>>() != null)
+                if (Traverse.Create(__instance).Field("currentSort").GetValue<Comparison<InventoryItemElement>>() != null)
                 {
-                    SaveScrollPosition(__instance);
+                    m_scrollPos = Scrollbar(__instance).verticalNormalizedPosition;
+                    //Log("saving scroll (hover)");
                 }
             }
         }
 
-        [HarmonyPatch(typeof(BattleTech.UI.MechLabInventoryWidget), "ApplySorting")]
+        [HarmonyPatch(typeof(MechLabInventoryWidget), "ApplySorting")]
         public static class BattleTech_UI_MechLabInventoryWidget_ApplySorting_Postfix
         {
-            public static void Postfix(BattleTech.UI.MechLabInventoryWidget __instance)
+            public static void Postfix(MechLabInventoryWidget __instance)
             {
                 if (__instance.gameObject.activeInHierarchy)
                 {
@@ -57,37 +99,137 @@ namespace StopScrolling
             }
         }
 
-        [HarmonyPatch(typeof(BattleTech.UI.MechLabInventoryWidget), "ApplyFiltering")]
-        public static class BattleTech_UI_MechLabInventoryWidget_ApplyFiltering_Patch
+        [HarmonyPatch(typeof(MechLabInventoryWidget), "ApplyFiltering")]
+        public static class BattleTech_UI_MechLabInventoryWidget_ApplyFiltering_Postfix
         {
-            public static void Postfix(BattleTech.UI.MechLabInventoryWidget __instance)
+            public static void Postfix(MechLabInventoryWidget __instance)
             {
-                if (__instance.gameObject.activeInHierarchy)
+                // when a filter is applied, permit the default behaviour (scroll to top) unless we're in the mech bay
+                if (m_scrollAfterFilter)
                 {
-                    __instance.StartCoroutine(EndOfFrameScrollBarMovement(__instance));
+                    m_scrollAfterFilter = false;
+                    if (__instance.gameObject.activeInHierarchy)
+                    {
+                        __instance.StartCoroutine(EndOfFrameScrollBarMovement(__instance));
+                    }
                 }
+                //Log($"resetting selection index");
+                m_selectedIdx = -1; // reset selection on filter as it will scroll to the top
             }
         }
 
-        private static IEnumerator EndOfFrameScrollBarMovement(BattleTech.UI.MechLabInventoryWidget __instance)
+        private static IEnumerator EndOfFrameScrollBarMovement(MechLabInventoryWidget __instance)
         {
             yield return new WaitForEndOfFrame();
-            Traverse.Create(__instance).Field("scrollbarArea").Property("verticalNormalizedPosition").SetValue(Mathf.Clamp(StopScrolling.m_pos, 0f, 1f));
+            Scrollbar(__instance).verticalNormalizedPosition = Mathf.Clamp(m_scrollPos, 0f, 1f);
             yield break;
         }
 
-        private static void SaveScrollPosition(BattleTech.UI.MechLabInventoryWidget __instance)
+        private static ScrollRect Scrollbar(MechLabInventoryWidget __instance)
         {
-            if (StopScrolling.m_pos == -1)
-            {
-                StopScrolling.m_pos = 1f;
-            }
-            else
-            {
-                StopScrolling.m_pos = Traverse.Create(__instance).Field("scrollbarArea").Property("verticalNormalizedPosition").GetValue<float>();
-            }
-            //Log($"saved scroll position {StopScrolling.pos}");
+            return Traverse.Create(__instance).Field("scrollbarArea").GetValue<ScrollRect>();
         }
+
+        private static void ScrollToTop(MechLabInventoryWidget __instance)
+        {
+            m_scrollPos = 1f;
+            Scrollbar(__instance).verticalNormalizedPosition = 1f;
+        }
+
+        #endregion MechLabInventoryWidget
+
+        #region SG_Shop_Screen
+
+        private static int m_selectedIdx = -1;
+        private static bool m_needSaveSelected;
+
+        [HarmonyPatch(typeof(SG_Shop_Screen), "ChangeToBuy")]
+        public static class BattleTech_UI_SG_Shop_Screen_ChangeToBuy_Postfix
+        {
+            public static void Postfix(SG_Shop_Screen __instance)
+            {
+                ScrollToTop(Traverse.Create(__instance).Field("inventoryWidget").GetValue<MechLabInventoryWidget>());
+            }
+        }
+
+        [HarmonyPatch(typeof(SG_Shop_Screen), "ChangeToSell")]
+        public static class BattleTech_UI_SG_Shop_Screen_ChangeToSell_Postfix
+        {
+            public static void Postfix(SG_Shop_Screen __instance)
+            {
+                ScrollToTop(Traverse.Create(__instance).Field("inventoryWidget").GetValue<MechLabInventoryWidget>());
+            }
+        }
+
+        [HarmonyPatch(typeof(SG_Shop_Screen), "OnItemSelected")]
+        public static class BattleTech_UI_SG_Shop_Screen_OnItemSelected_Prefix
+        {
+            public static void Prefix(SG_Shop_Screen __instance, InventoryItemElement item)
+            {
+                if (m_needSaveSelected)
+                {
+                    m_selectedIdx = GetActiveItems(__instance).IndexOf(item);
+                    //Log($"saving index {m_selectedIdx}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(SG_Shop_Screen), "RefreshSelection")]
+        public static class BattleTech_UI_SG_Shop_Screen_EndOfFrameRefreshSelection_Prefix
+        {
+            public static void Prefix(SG_Shop_Screen __instance)
+            {
+                m_needSaveSelected = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(SG_Shop_Screen), "RefreshSelection")]
+        public static class BattleTech_UI_SG_Shop_Screen_EndOfFrameRefreshSelection_Postfix
+        {
+            public static void Postfix(SG_Shop_Screen __instance)
+            {
+                ReSelect(__instance);
+                m_needSaveSelected = true;
+            }
+        }
+
+        public static List<InventoryItemElement> GetActiveItems(SG_Shop_Screen __instance)
+        {
+            return Traverse.Create(__instance).Field("inventoryWidget").Field("localInventory").GetValue<List<InventoryItemElement>>().FindAll(i => i.isActiveAndEnabled);
+        }
+
+        private static void ReSelect(SG_Shop_Screen __instance)
+        {
+            //Log($"attempting to select index {m_selectedIdx}");
+            if (m_selectedIdx > -1)
+            {
+                List<InventoryItemElement> list = GetActiveItems(__instance);
+                if (list.Count > 0)
+                {
+                    if (m_selectedIdx > list.Count - 1)
+                    {
+                        m_selectedIdx = list.Count - 1;
+                    }
+                    if (m_selectedIdx > -1)
+                    {
+                        //Log($"selecting item at index {m_selectedIdx}");
+                        InventoryItemElement item = list[m_selectedIdx];
+                        if (item != null)
+                        {
+                            if (item.gameObject.activeSelf)
+                            {
+                                item.buttonElement.ForceRadioSetSelection();
+                            }
+                            __instance.OnItemSelected(item);
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        #endregion SG_Shop_Screen
 
     }
 }
